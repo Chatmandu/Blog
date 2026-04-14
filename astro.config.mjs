8659daad5a1de5b93fa362eedbbebac3d17be7eb
Niall Walsh <nbwalsh@gmail.com>
2026-04-14 09:22:04 +0100

    Image SEO tweaks and site map generatione

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://manualmode.xyz',
   output: 'static',
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
   markdown: {
     shikiConfig: {
       theme: 'github-light',
